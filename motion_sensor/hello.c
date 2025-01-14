63c7da18c75dbcec0deb20e5fe0a9b5ee8033680
Christopher Odom <christopher.r.odom@gmail.com>
2025-01-14 18:25:09 +0000

    Succesfull crgb measurements

--- motion_sensor/hello.c
+++ motion_sensor/hello.c
@@ -1,9 +1,14 @@
 #include <linux/module.h>
+#include <linux/delay.h>
 #include <linux/miscdevice.h>
 #include <linux/i2c.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include <linux/cdev.h>
+
+#include <linux/wait.h>
+#include <linux/timer.h>
+#include <linux/jiffies.h>
 
 #define APDS_MAJOR 42
 #define APDS_MAX_MINORS 1
@@ -15,7 +20,10 @@
 #define APDS_PDATA 0x90 // prox data
 #define APDS_PILT 0x89
 #define APDS_PIHT 0x8B
-#define APDS_ALS_VALID 0x93
+#define APDS_STATUS 0x92
+#define APDS_WAIT_TIME 0x83
+#define APDS_ADC_TIME 0x82
+#define APDS_CONTROL 0x8f
 
 /*
  * Color data is reported using two bytes, one
@@ -35,15 +43,21 @@
 #define APDS_BDATAL 0x9A
 #define APDS_BDATAH 0x9B
 
-#define APDS_ON (1)
+// Enable bitfields 
+#define APDS_ON_ENABLE (1)
+#define APDS_ALS_ENABLE (1<<1)
 #define APDS_PROX_ENABLE (1<<2)
 #define APDS_WAIT_ENABLE (1<<3)
-#define APDS_AI_ENABLE (1<<4)
+#define APDS_ALS_INT_ENABLE (1<<4)
 #define APDS_PROX_INT_ENABLE (1<<5)
+#define APDS_GESTURE_ENABLE (1<<6)
 
 struct apds9960_dev {
   struct i2c_client* client;
   struct miscdevice apds9960_miscdevice;
+  wait_queue_head_t wq;
+  bool color_ready;
+  struct timer_list timer;
   char name[8]; /* apds9960 */
 };
 
@@ -58,6 +72,14 @@ static const struct i2c_device_id i2c_ids[] = {
   { }
 };
 MODULE_DEVICE_TABLE(i2c, i2c_ids);
+
+/* Timer callback that marks data as ready and wakes waiting processes */
+static void sensor_timer_cb(struct timer_list *t)
+{
+    struct apds9960_dev *dev = from_timer(dev, t, timer);
+    dev->color_ready = true;
+    wake_up(&dev->wq);
+}
 
 static ssize_t apds9960_write_file(struct file *file, const char __user *userbuf,
     size_t count, loff_t *ppos)
@@ -83,39 +105,59 @@ static ssize_t apds9960_write_file(struct file *file, const char __user *userbuf
 static ssize_t apds9960_read_file(struct file *file, char __user *userbuf,
     size_t count, loff_t *ppos)
 {
-  int expval, size, pvalid, avalid, enable;
-  unsigned char red_low, red_high, green_low, green_high, blue_low, blue_high;
-  char buf[50]; // Increase buffer size for RGB values
+  int expval, size, pvalid, avalid, enable, ret;
+  unsigned char clear_low, clear_high, red_low, red_high, green_low, green_high, blue_low, blue_high;
+  char buf[60]; // Increase buffer size for RGB values
   struct apds9960_dev * apds9960;
   apds9960 = container_of(file->private_data, struct apds9960_dev, apds9960_miscdevice);
-  i2c_smbus_write_byte_data(apds9960->client, APDS_ENABLE, APDS_ON | APDS_PROX_ENABLE | APDS_AI_ENABLE);
-  pvalid = i2c_smbus_read_byte_data(apds9960->client, APDS_PDATA);
+
+  apds9960->color_ready = false;
+  i2c_smbus_write_byte_data(apds9960->client, APDS_ENABLE, APDS_ON_ENABLE | APDS_PROX_ENABLE | APDS_ALS_ENABLE);
+  i2c_smbus_write_byte_data(apds9960->client, APDS_ADC_TIME, 0xff);
+  // Sets als gain control
+  i2c_smbus_write_byte_data(apds9960->client, APDS_CONTROL, 1);
+  pvalid = i2c_smbus_read_byte_data(apds9960->client, APDS_STATUS) & (1<<1);
   if(pvalid) {
     pr_info("Reading prox data\n");
     expval = i2c_smbus_read_byte_data(apds9960->client, PROX_DATA_REG);
     if (expval < 0) {
         return -EFAULT;
     }
+  } else {
+    pr_info("Prox not valid, skipping!!!!");
   }
-  avalid = 1; // i2c_smbus_read_byte_data(apds9960->client, APDS_ALS_VALID);
+
+  mod_timer(&apds9960->timer, msecs_to_jiffies(30));
+  ret = wait_event_interruptible(apds9960->wq, apds9960->color_ready);
+  if(ret)
+    return ret;
+
+  avalid = i2c_smbus_read_byte_data(apds9960->client, APDS_STATUS) & 1;
   if(avalid) {
     pr_info("Reading color data\n");
     // Read RGB values
+    clear_low = i2c_smbus_read_byte_data(apds9960->client, APDS_CDATAL);
+    clear_high = i2c_smbus_read_byte_data(apds9960->client, APDS_CDATAH);
     red_low = i2c_smbus_read_byte_data(apds9960->client, APDS_RDATAL);
     red_high = i2c_smbus_read_byte_data(apds9960->client, APDS_RDATAH);
     green_low = i2c_smbus_read_byte_data(apds9960->client, APDS_GDATAL);
     green_high = i2c_smbus_read_byte_data(apds9960->client, APDS_GDATAH);
     blue_low = i2c_smbus_read_byte_data(apds9960->client, APDS_BDATAL);
     blue_high = i2c_smbus_read_byte_data(apds9960->client, APDS_BDATAH);
+  } else {
+    pr_info("Color not valid, skipping!!!!");
   }
+
+  // Test if both failed
   if(!avalid && !pvalid) {
       pr_info("Data not ready!\n");
       return -EFAULT;
   }
-  // Prepare the output buffer with RGB values
+
+  // Prepare the output buffer
   if(avalid)
-    size = sprintf(buf, "ExpVal: %02x, RGB: R(%02x%02x) G(%02x%02x) B(%02x%02x)",
-                 expval, red_high, red_low, green_high, green_low, blue_high, blue_low);
+    size = sprintf(buf, "ExpVal: %02x, CRGB: C(%02x%02x) R(%02x%02x) G(%02x%02x) B(%02x%02x)",
+                 expval, clear_high, clear_low, red_high, red_low, green_high, green_low, blue_high, blue_low);
   /*
    * Replace NULL by \n. It is not needed to have a char array
    * ended with \0 character.
@@ -149,6 +191,11 @@ static int apds9960_probe (struct i2c_client * client)
   i2c_set_clientdata(client,apds9960);
   /* Store pointer to I2C client */
   apds9960->client = client;
+
+
+  /* Wait mechanism */
+  init_waitqueue_head(&apds9960->wq);
+  timer_setup(&apds9960->timer, sensor_timer_cb, 0);
 
   /*
    * Initialize the misc device, apds9960 is incremented
